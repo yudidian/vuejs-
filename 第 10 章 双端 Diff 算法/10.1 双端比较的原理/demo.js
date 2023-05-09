076436ebd582aac26e5582712c9a18af9e49d58d
denghongyu <1848761082@qq.com>
2023-05-09 14:46:00 +0800

    测试合并

--- 第 10 章 双端 Diff 算法/10.1 双端比较的原理/demo.js
+++ 第 10 章 双端 Diff 算法/10.1 双端比较的原理/demo.js
@@ -70,6 +70,7 @@ function createRenderer() {
         newEndVNode = newChildren[--newEndIdx]
       } else if (oldEndVNode.key === newStartVNode.key) {
         // 新节点的头部与旧节点的尾部key相同，将旧节点尾部移动到旧节点头部
+        // 66666666
         patch(oldEndVNode, newStartVNode, container)
         insert(oldEndVNode.el, container, oldStartVNode.el)
         oldEndVNode = oldChildren[--oldEndIdx]
